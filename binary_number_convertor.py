529ef5b418d1b0c6aae59338db01e94cb3f0a32f
Brian Welch <myself@brianwelch.se>
2017-09-12 15:22:02 +0200

    Update binary_number_convertor.py

--- binary_number_convertor.py
+++ binary_number_convertor.py
@@ -9,13 +9,16 @@
 def binary_calc(digit_in):
     '''this one took a while - converting an integer into a binary'''
     binary_digit1 = []
-    while digit_in > 0:
-        binar = digit_in%2
-        binary_digit1 += str(binar)
-        digit_in = int(digit_in / 2)
-    binary_length = int(len(binary_digit1))
     output = ""
-    while binary_length > 0:
-        output += binary_digit1[binary_length - 1]
-        binary_length = binary_length - 1
+    if (digit_in == 0):
+        output = 0
+    else:
+        while digit_in > 0:
+            binar = digit_in%2
+            binary_digit1 += str(binar)
+            digit_in = int(digit_in / 2)
+        binary_length = int(len(binary_digit1))
+        while binary_length > 0:
+            output += binary_digit1[binary_length - 1]
+            binary_length = binary_length - 1
     return output
